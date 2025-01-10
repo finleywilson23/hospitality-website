4996af69b46d7f9d469c40c753920c0924be069d
finleywilson23 <146830771+finleywilson23@users.noreply.github.com>
2025-01-10 17:03:28 +0200

    Update slides.js
    reed previous comment

--- slides.js
+++ slides.js
@@ -1,13 +1,28 @@
-let slideIndex =0;
-showSlides();
+document.addEventListener("DOMContentLoaded",function (){
 
-function shoSlides(){
-	let i;
-	let slides = document.getElementsByClassName("myslides")l
-	for (i = 0; i < slides.length; i++){
-    	slides[i].style.display = "none";
-}
-slideIndex++;
-if (slideIndex > slides.length) {slideIndex = 1}
-slides[slidesIndex -1].style.display="block;
-setTimeout(showSlides,3000);
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+
+        let slides = document.getElementsByClassName("myslides");
+        console.log("Slides found:",slides.length);
+        if (slides.length === 0) {
+            console.error("no slides found");
+            return;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+
+        setTimeout(showSlides, 3000);
+    }
+
+});
